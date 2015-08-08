828776b5c1791faedc70e6d1e633da434abdc870
Shrikant Patnaik <shrikant.patnaik@gmail.com>
2015-08-08 21:01:20 +0800

    updated octoprint to show time left

--- jobs/octoprint.rb
+++ jobs/octoprint.rb
@@ -15,10 +15,18 @@ SCHEDULER.every '30s', :first_in => 0 do |job|
     req.headers['X-Api-Key'] = config['octoprint']['api_key']
   end
   parsed_response = JSON.parse(resp.body)
+  print_time = parsed_response['progress']['printTimeLeft']
+  time_left = if !print_time.nil? and print_time.is_a? Integer
+    Time.at(print_time).utc.strftime("%H:%M:%S")
+  else
+    "unknown"
+  end
+
   data = {
     progress:  "#{(parsed_response['progress']['completion'] * 100).round.to_f/100}%",
     state: parsed_response['state'],
-    job: parsed_response['job']['file']['name']
+    job: parsed_response['job']['file']['name'],
+    time_left: time_left
   }
   send_event('octoprint', data)
 end
